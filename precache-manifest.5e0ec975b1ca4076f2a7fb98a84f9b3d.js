26a1135fffed533db06a0101366d98ff74c30ef8
Adrielle Moraes <adrielle.nazar@hotmail.com>
2020-10-06 00:08:47 +0100

    Updates

---
+++ precache-manifest.5e0ec975b1ca4076f2a7fb98a84f9b3d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4422b3129e5f69b91b7c675271ba985",
+    "url": "/app-masters-dashboard/index.html"
+  },
+  {
+    "revision": "bbe2739cd62030553003",
+    "url": "/app-masters-dashboard/static/css/2.30cbe4fd.chunk.css"
+  },
+  {
+    "revision": "b139538c5883752ecde9",
+    "url": "/app-masters-dashboard/static/css/main.90d6e3f5.chunk.css"
+  },
+  {
+    "revision": "bbe2739cd62030553003",
+    "url": "/app-masters-dashboard/static/js/2.9277f158.chunk.js"
+  },
+  {
+    "revision": "05f87c2f2c6ecc301beeeff1fd817d48",
+    "url": "/app-masters-dashboard/static/js/2.9277f158.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b139538c5883752ecde9",
+    "url": "/app-masters-dashboard/static/js/main.b7ef8f2e.chunk.js"
+  },
+  {
+    "revision": "0c691da4f64ce65e1e25",
+    "url": "/app-masters-dashboard/static/js/runtime-main.43d02c9a.js"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/app-masters-dashboard/static/media/sidebar-1.8e9f0a3c.jpg"
+  }
+]);
